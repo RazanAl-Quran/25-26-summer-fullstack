b8cf83deeea26383bf839cdcc72afa558297b536
RazanAl-Quran <quraan_razan@hotmail.com>
2026-08-31 17:45:22 +0300

    final crud fullstack app has been added

---
+++ 04-fullstack/demo-02/store-server/routes/products.js
@@ -0,0 +1,42 @@
+import express from "express";
+import db from "../db.js";
+import adminAuth from "../middleware/adminAuth.js";
+const router = express.Router();
+
+// localhost:5000/api/products/
+router.get("/", async (req, res) => {
+  const result = await db.query("SELECT * FROM products ORDER BY id");
+  res.json(result.rows);
+});
+
+// localhost:5000/api/products/
+// body name price, header x-role
+router.post("/", adminAuth, async (req, res) => {
+  const { name, price } = req.body;
+  const result = await db.query(
+    "INSERT INTO products (name, price) VALUES ($1, $2) RETURNING *",
+    [name, price]
+  );
+  res.status(201).json(result.rows[0]);
+});
+
+router.put("/:id", adminAuth, async (req, res) => {
+  const { name, price } = req.body;
+  const result = await db.query(
+    "UPDATE products SET name = $1, price = $2 WHERE id = $3 RETURNING *",
+    [name, price, req.params.id]
+  );
+  result.rows.length > 0
+    ? res.json(result.rows[0])
+    : res.status(404).json({ message: "Product not found" });
+});
+
+// localhost:5000/api/products/2
+router.delete("/:id", adminAuth, async (req, res) => {
+  const result = await db.query("DELETE FROM products WHERE id = $1 RETURNING *", [req.params.id]);
+  result.rows.length > 0
+    ? res.json({ deleted: result.rows[0] })
+    : res.status(404).json({ message: "Product not found" });
+});
+
+export default router;
---
+++ 04-fullstack/demo-02/store-server/server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.js";
+import productRoutes from "./routes/products.js";
+import userRoutes from "./routes/users.js";
+import db from './db.js';
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+// app.use(cors({
+//   origin: 'http://localhost:5142' // only allow React frontend
+// }));
+
+// localhost:5000
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+
+app.get("/", (req, res) => {
+  res.send("✅ Store API (PostgreSQL) is running");
+});
+
+
+db.connect().then(() => {
+  app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+})
